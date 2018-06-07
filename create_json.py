e8195e2e6e39e1a06d34d8121bc17c67dac24dad
Anna Stalknecht <ajstalknecht@gmail.com>
2018-06-07 16:10:54 +0200

    logboek

--- create_json.py
+++ create_json.py
@@ -54,6 +54,6 @@ def nice_indent(from_file, to_file):
 		json.dump(copydata, f, indent=2)
 
 
-#delete_columns('gunfire_small.csv')
+# delete_columns('gunfire_small.csv')
 csv_to_json('gunfire_small.csv', 'gunfire_small.json')
 nice_indent('gunfire_small.json', 'gunfire_small.json')
